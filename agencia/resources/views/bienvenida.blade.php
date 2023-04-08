6dff59cc6f7bc2c68e03465091e6495c8f825e7b
Marcos <exegeses@gmail.com>
2023-04-08 13:45:42 -0300

    creación de proyecto agencia

---
+++ agencia/resources/views/bienvenida.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Proyecto Laravel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body>
+    <header class="py-4 bg-dark"></header>
+    <main class="container py-4">
+        <h1>Soy la vista de bienvenida</h1>
+    </main>
+    <footer class="py-5 text-center bg-light fixed-bottom">
+        leyenda de copyright
+    </footer>
+</body>
+</html>
